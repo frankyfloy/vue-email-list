bb7914e976873ab472b28f2bb4d36ff2b55da80c
Francesco Sica <fsica171@gmail.com>
2021-04-08 16:41:27 +0200

    Inizio esercizio

---
+++ assets/main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        arrEmail : [],
+        numEmail : 10,
+    },
+
+
+
+    methods: {
+        emailGenerator :function () {
+            for (var i = 0; i < this.numEmail; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response)  => {
+                    console.log(response.data);
+                    if (response.data.success) {
+                        this.arrEmail.push(response.data.response)
+                        console.log(this.arrEmail);
+                    }
+
+                    // else {
+                    //     numEmail + 1;
+                    // }
+                });
+
+            }
+
+        }
+    },
+});
